495e1224207e3e065351a64d4489f3ab804aec1c
Oleg Sazonov <sazonov1994@gmail.com>
2022-10-17 14:40:29 +0300

    Created project by Spring Boot Data

---
+++ src/main/java/com/olegsazonov/springboot/spring_data_jpa/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.olegsazonov.springboot.spring_data_jpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+}
